962a1de788b2fcf6c8dbe14d8ece951ffd25984f
Pedro Alano <pedroalano@gmail.com>
2025-12-18 21:47:20 -0300

    feat: integrate categories routes into main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const categoriesRoutes = require("./categories.routes");
 const router = express.Router();
 
 router.get("/health", (req, res) => {
   res.json({ status: "I'm live!" });
 });
+
+router.use("/categories", categoriesRoutes);
 
 module.exports = router;
